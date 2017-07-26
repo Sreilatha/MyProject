4a29208c8cc9db6c9efd8e312712273ac41b1da8
srilatha g <gsrilatha.42@gmail.com>
2017-07-25 18:20:37 -0700

    added HelloWorld

---
+++ src/com/srilatha/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.srilatha;
+
+/**
+ * Created by Srilatha on 7/25/2017.
+ */
+public class HelloWorld {
+    public static void main(String [] args){
+        System.out.println("Hello world");
+    }
+}
