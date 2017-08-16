45f8a1e316b173a194fa1e55e6977693181fdf3d
Srilatha <gsrilatha.42@gmail.com>
2017-08-15 17:35:39 -0700

    add implementation for binary search

--- src/com/srilatha/BinarySearch.java
+++ src/com/srilatha/BinarySearch.java
@@ -1,4 +1,23 @@
 package com.srilatha;
 
 public class BinarySearch {
+    public static void main(String[] args){
+        System.out.println(binarySearch(new int[]{9,11,15,18,22,31},18));
+
+}
+public static int binarySearch(int[] a,int x){
+    int p=0;
+    int r=a.length-1;
+    while(p<=r){
+        int q=(p+r)/2;
+        if (x<a[q]) {
+            r = q - 1;
+
+        }else if(x>a[q]) {
+            p = q + 1;
+
+        }else return q;
+    }
+return -1;
+}
 }
