f42aee4de4e7e6499081cf215b31fdea48251d3b
Srilatha <gsrilatha.42@gmail.com>
2017-08-17 16:35:56 -0700

    SelectionSort.

--- src/com/srilatha/SelectionSort.java
+++ src/com/srilatha/SelectionSort.java
@@ -1,4 +1,28 @@
 package com.srilatha;
 
 public class SelectionSort {
+    public static void main(String[] args){
+        int[] array=selectionSort(new int[]{11,9,3,6,14,34});
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
+
+    }
+
+
+
+    public static int[] selectionSort(int[] a){
+        for(int i=0;i<a.length;i++){
+            int minimum=i;
+            for(int j=i+1;j<a.length;j++){
+                if(a[j]<a[minimum]){
+                    minimum=j;
+                }
+            }
+            int temp=a[i];
+            a[i]=a[minimum];
+            a[minimum]=temp;
+        }
+        return a;
+    }
 }
